784d18ec007a9dd8cb2c34de4def59686139f838
Dylan Fox <dylan.fox@colorado.edu>
2019-12-05 12:18:17 -0700

    transfering storage code to worker

--- worker/worker_server.py
+++ worker/worker_server.py
@@ -5,8 +5,35 @@ import sys
 import jsonpickle
 import io
 import redis
+import hashlib
 import socket
 from neural_style_mod import transform
+
+
+def upload_picture_to_bucket(image_bytes, hash_value):
+    """
+    This function uploads an image to the a gcp storage bucket.
+    The storage blob is named by the value given in hash value.
+
+    Parameter:
+    -image_bytes(bytes): the image to upload
+    -hash_value(str): md5 hash value of image bytes. Blob is named after this in gcp cloud storage.
+    """
+
+    blob_name = hash_value
+    bucket_name = 'style-transfer-images'
+    storage_client = storage.Client()
+    bucket = None
+    buckets = storage_client.list_buckets()
+    bucket_names = []
+    for bucket in buckets:
+        bucket_names.append(bucket.name)
+    if bucket_name not in bucket_names:
+        bucket = storage_client.create_bucket(bucket_name)
+    else:
+        bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(image_bytes, content_type='image/jpg')
 
 def send_to_redis(key, value, db_number):
     """
@@ -36,7 +63,7 @@ def get_from_redis(key, db_number):
 
 def send_to_logs(message):
     """
-    This function takes a log message and sends it to a RabbitMQ "rest.debug" topic
+    This function takes a log message and sends it to a RabbitMQ "worker.debug" topic
     hosted on the "rabbitmq" VM using "logs" exchange.
 
     Parameters:
@@ -50,7 +77,7 @@ def send_to_logs(message):
     channel.exchange_declare(exchange='logs', exchange_type='topic')
     channel.basic_publish(
         exchange='logs',
-        routing_key='rest.debug',
+        routing_key='worker.debug',
         body=message,
         properties=pika.BasicProperties(
             delivery_mode=2,
@@ -69,8 +96,14 @@ def callback(ch, method, properties, body):
     """
     data = jsonpickle.decode(body)
     try:
-        transform(data["content"], data["style"], data["output_file"])
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+        transformed_image = transform(data["content"], data["style"])
+        hash_value = data['hash']
+        upload_picture_to_bucket(transformed_image, hash_value)
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    except Exception as e:
+        send_to_logs(e)
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='rabbitmq'))
