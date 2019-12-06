dd8a0309d212b793f0e49cfc058929ac86d704c0
Dylan Fox <dylan.fox@colorado.edu>
2019-12-06 11:11:43 -0700

    Changing rest server and client to pass raw image bytes and cleaning up code

--- rest/rest_client.py
+++ rest/rest_client.py
@@ -1,63 +1,61 @@
-from __future__ import print_function
-import requests
-import json
+
 import argparse
-import time
-from statistics import mean
+import io
+import json
 import jsonpickle
-#import redis
-import os.path
-import scipy.misc
-import numpy as np
+from PIL import Image
+import requests
 
-def imread(path):
-    img = scipy.misc.imread(path).astype(np.float)
-    if len(img.shape) == 2:
-        # grayscale
-        img = np.dstack((img,img,img))
-    elif img.shape[2] == 4:
-        # PNG with alpha channel
-        img = img[:,:,:3]
-    return img
 
-def upload_pics(address, content_file, style_file, output_file):
-    data = {"style": imread(style_file), "content": imread(content_file),
-            "output_file": output_file}
+def upload_pics(address, content_file_path, style_file_path):
 
-    image_url = address + '/image/'+os.path.basename(content_file)
-    return requests.put(image_url, data=jsonpickle.encode(data))
+    #get raw image from content file and style file
+    byte_stream = io.BytesIO()
+    content_image = Image(content_file_path)
+    content_image.save(byte_stream, format='JPEG')
+    content_bytes = byte_stream.getvalue()
 
-def main(server_address, endpoint, content_file, style_file, output_file):
+    byte_stream = io.BytesIO()
+    style_image = Image(style_file_path)
+    style_image.save(byte_stream, format="JPEG")
+    style_bytes = byte_stream.get
+
+
+    data = {"style":style_bytes , "content":content_bytes}
+    
+    return requests.put(address, data=jsonpickle.encode(data))
+
+def main(server_address, endpoint, content_file, style_file):
     address = 'http://'+server_address+':5000'
 
     if endpoint == "image":
+        address += '/image'
         response = upload_pics(address, content_file,
-                               style_file, output_file).json()
+                               style_file).json()
         print(response)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="REST API")
+    parser = argparse.ArgumentParser(description="Makes calls to style transfer service. \
+        Returns image hash, which can be used to retrieve image with style transfered.")
 
     parser.add_argument('server_address',
                         type=str,
-                        help='Address of the server ex. localhost')
+                        help='Address of the server.')
+
+    parser.add_argument('content_file',
+                        type=str,
+                        help='The file to transform.')
+
+    parser.add_argument('style_file',
+                        type=str,
+                        help='The source file for the style.')
 
     parser.add_argument('endpoint',
                         type=str,
-                        help='The endpoint of the server we wish to query')
-
-    parser.add_argument('--content_file',
-                        type=str,
-                        help='The file we wish to transform')
-
-    parser.add_argument('--style_file',
-                        type=str,
-                        help='The source file for the style')
-
-    parser.add_argument('--output_file',
-                        type=str,
-                        help='The output file for the transformed image')
+                        default="image"
+                        help='The endpoint of the server to query.')
 
     args = parser.parse_args()
+
     main(args.server_address, args.endpoint, args.content_file,
-         args.style_file, args.output_file)
+         args.style_file)
