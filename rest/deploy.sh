0f2d50dfc6a49b1fb696e7e0b0437c6451687da3
Dylan Fox <dylan.fox@colorado.edu>
2019-12-08 12:18:53 -0700

    updating rest deployment script

--- rest/deploy.sh
+++ rest/deploy.sh
@@ -1,4 +1,6 @@
-docker build . -t gcr.io/csci5253/rest-server:v1
-docker push gcr.io/csci5253/rest-server:v1
-kubectl create deployment rest-server --image=gcr.io/csci5253/rest-server:v1
+#takes in gcr.io/csci5253/rest-server:v1
+docker build . -t $1
+docker push $1
+kubectl create deployment rest-server --image=$1
 kubectl expose deployment rest-server --type=LoadBalancer --port=5000
+kubectl set image deployment rest-server rest-server=$1
--- worker/deployment_script.sh
+++ worker/deployment_script.sh
@@ -1,5 +1,9 @@
-docker build . -t gcr.io/csci5253/worker-server:v3
-docker push gcr.io/csci5253/worker-server:v3
-#use below to install device drivers.
-#kubectl create -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/k8s-1.9/nvidia-driver-installer/cos/daemonset-preloaded.yaml
+#Takes name of remote repo/image to use. eg.
+#./deployment_script.sh gcr.io/csci5253/worker-server:v5
+
+docker build . -t $1
+docker push $1
+#daemon pod for gpu drivers
+kubectl create -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/k8s-1.9/nvidia-driver-installer/cos/daemonset-preloaded.yaml
 kubectl create -f worker-pod.yaml
+kubectl set image -f worker-pod.yaml worker-server=$1
